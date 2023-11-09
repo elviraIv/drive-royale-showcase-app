c90475d33dd8ecdd0adc06887dac71d17d92bccc
Elvira <elvira.n.ivanova@gmail.com>
2023-11-09 11:36:29 +0200

    Add media. Modify CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,24 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import Image from "next/image";
-import { CustomButtonProps } from '@/types';
+import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({ title, containerStyles, handleClick} : CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType
+}: CustomButtonProps) => {
   return (
-      <button
+    <button
       disabled={false}
-      type={'button'}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
-      >
-        <span className={`flex-1`}>
-          {title}
-        </span>
-      </button>
-    )
-}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
