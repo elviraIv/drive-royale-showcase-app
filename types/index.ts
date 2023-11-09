0acf9906ed998bc31ecfaed4d715dc69545b3739
Elvira <elvira.n.ivanova@gmail.com>
2023-11-09 15:33:57 +0200

    Modified types

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,31 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string,
-    containerStyles?: string,
-    btnType?:'button' | 'submit'
-    handleClick?: MouseEventHandler<HTMLButtonElement>
+    title: string;
+    containerStyles?: string;
+    btnType?: 'button' | 'submit';
+    textStyles?:string;
+    rightIcon?:string;
+    isDisabled?:boolean;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface SearchManufacturerProps {
-    manufacturer:string,
-    setManufacturer:(manufaturer:string) => void;
+    manufacturer: string;
+    setManufacturer: (manufaturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
