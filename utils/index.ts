4c2c7fa313cd4c27c75f4df017b2ef806a8ce237
Elvira <elvira.n.ivanova@gmail.com>
2023-11-10 11:22:35 +0200

    Update searching

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     'X-RapidAPI-Key': 'bf174c1b3dmsh50f6ed711f149a6p1148bejsn0f29d4f982e6',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers
   });
 
